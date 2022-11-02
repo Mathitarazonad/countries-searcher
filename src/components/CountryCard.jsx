5357a26e47c8acf70f662ca7647f39602f01aa55
Mathias Tarazona <vladitarazona@gmail.com>
2022-11-02 11:34:10 -0300

    Created initial country card component

---
+++ src/components/CountryCard.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function CountryCard() {
+  return (
+    <article className='country-card'>
+      <img />
+      <section className='country-info'>
+        <h2>Germany</h2>
+        <p><b>Population: </b>81,770,900</p>
+        <p><b>Region: </b>Europe</p>
+        <p><b>Capital: </b>Berlin</p>
+      </section>
+    </article>
+  )
+}
+
+export default CountryCard;
