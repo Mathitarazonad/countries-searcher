56a0e8d36e73727a962c7790ce2f99fb16fb6d4a
Mathias Tarazona <vladitarazona@gmail.com>
2022-11-03 15:33:36 -0300

    Gave unique key id to each single country card

--- src/components/CountriesList.jsx
+++ src/components/CountriesList.jsx
@@ -2,23 +2,28 @@ import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { fetchCountries, setCountriesToRender } from '../store/countriesSlice';
 import CountryCard from './CountryCard';
+import { v4 as uuidv4 } from 'uuid';
 
 function CountriesList() {
   const dispatch = useDispatch();
-  const countriesToRender = useSelector(store => store.countries.countriesToRender);
-  const fetchedCountries = useSelector(store => store.countries.fetchedCountries)
+  const countriesToRender = useSelector(
+    (store) => store.countries.countriesToRender
+  );
+  const fetchedCountries = useSelector(
+    (store) => store.countries.fetchedCountries
+  );
 
   useEffect(() => {
-    dispatch(fetchCountries(countriesToRender))
-  }, [countriesToRender])
+    dispatch(fetchCountries(countriesToRender));
+  }, [countriesToRender]);
 
   return (
-    <div className='countries-container'>
-      {fetchedCountries.map(
-        country => <CountryCard country={country}/>
-      )}
+    <div className="countries-container">
+      {fetchedCountries.map((country) => (
+        <CountryCard country={country} key={uuidv4()} />
+      ))}
     </div>
-  )
+  );
 }
 
 export default CountriesList;
