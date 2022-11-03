3ca7d726a4810c63e21fbf6a7a54a6d1578729c7
Mathias Tarazona <vladitarazona@gmail.com>
2022-11-03 18:04:26 -0300

    Added main font family and imported CountrySelected component to app for tests

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector} from 'react-redux';
 import Browser from './components/Browser';
 import Filter from './components/Filter';
 import Header from './components/Header';
 import './styles/main.css';
 import { getAllCountries } from './store/countriesSlice';
 import CountriesList from './components/CountriesList';
+import CountrySelected from './components/CountrySelected';
 
 function App() {
   const dispatch = useDispatch();
-
   useEffect(() => {
     dispatch(getAllCountries())
   }, [dispatch])
@@ -23,6 +23,7 @@ function App() {
           <Browser/>
           <Filter />
           <CountriesList />
+          <CountrySelected />
         </div>
       </main>
     </div>
