f7cea8265ab64c933ae42d0bb4f89f51dcbfaa4a
Mathias Tarazona <vladitarazona@gmail.com>
2022-11-04 15:37:36 -0300

    Added the respective information about the country selected, also the functionality to go back and click on border countries to select them

--- src/components/CountrySelected.jsx
+++ src/components/CountrySelected.jsx
@@ -1,31 +1,88 @@
 import React from 'react';
-import { BsFillReplyFill } from 'react-icons/bs'
+import { BsFillReplyFill } from 'react-icons/bs';
+import { v4 as uuidv4 } from 'uuid';
+import { useDispatch } from 'react-redux';
+import { setCountrySelected, setIfCountrySelected, } from '../store/countriesSlice';
 
-function CountrySelected({country}) {
+function CountrySelected({ country }) {
+  const dispatch = useDispatch();
+  const nativeNames = country.name.nativeName;
+  const currencie = country.currencies;
+  const languages = country.languages;
+
+  const handleClick = (country) => {
+    dispatch(setCountrySelected(country));
+  };
+
+  const handleBack = () => {
+    dispatch(setIfCountrySelected());
+    dispatch(setCountrySelected(''));
+  };
+
   return (
-    <div className='country-card-selected'>
-      <button><BsFillReplyFill />Back</button>
-      <img src='' alt='' />
-      <section className='__info'>
-        <h2></h2>
-        <div className='-geografy'>
-          <p><b>Native Name: </b></p>
-          <p><b>Population: </b>11.319.511</p>
-          <p><b>Region: </b>Europe</p>
-          <p><b>Sub Region: </b>Wester Europe</p>
-          <p><b>Capital: </b>Brussels</p>
+    <div className="country-card-selected">
+      <button onClick={() => handleBack()}>
+        <BsFillReplyFill />
+        Back
+      </button>
+      <img src={country.flags.png} alt="flag" />
+      <section className="__info">
+        <h2>{country.name.common}</h2>
+        <div className="-geografy">
+          <p>
+            <b>Native Name: </b>
+            {nativeNames[Object.getOwnPropertyNames(nativeNames)[0]].common}
+          </p>
+          <p>
+            <b>Population: </b>
+            {Intl.NumberFormat('en-US').format(country.population)}
+          </p>
+          <p>
+            <b>Region: </b>
+            {country.region}
+          </p>
+          <p>
+            <b>Sub Region: </b>
+            {country.subregion}
+          </p>
+          <p>
+            <b>Capital: </b>
+            {country.capital}
+          </p>
         </div>
-        <div className='-culture'>
-          <p><b>Top Level Domain: </b>.be</p>
-          <p><b>Currencies: </b>Euro</p>
-          <p><b>Languages: </b></p>
+        <div className="-culture">
+          <p>
+            <b>Top Level Domain: </b>
+            {country.tld}
+          </p>
+          <p>
+            <b>Currencies: </b>
+            {currencie[Object.getOwnPropertyNames(currencie)[0]].name}
+          </p>
+          <p>
+            <b>Languages: </b>
+            {Object.values(languages).join(', ')}
+          </p>
         </div>
-        <div className='-border-countries'>
-          <b>Border Countries:</b>
+        <div className="-border-countries">
+          <h3>
+            <b>Border Countries: </b>
+          </h3>
+          {country.hasOwnProperty('borders') ? (
+            <ul>
+              {country.borders.map((countryBorder) => (
+                <li key={uuidv4()} onClick={() => handleClick(countryBorder)}>
+                  {countryBorder}
+                </li>
+              ))}
+            </ul>
+          ) : (
+            <p>Has not Border Countries</p>
+          )}
         </div>
       </section>
     </div>
-  )
+  );
 }
 
 export default CountrySelected;
