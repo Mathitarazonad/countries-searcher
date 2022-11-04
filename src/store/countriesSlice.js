7209d7d18f42e650c9c307178f34323ae92fdbe1
Mathias Tarazona <vladitarazona@gmail.com>
2022-11-04 15:27:54 -0300

    Added country selected state and its reducer. Also modified the fetched countries to be sorted alphabetically

--- src/store/countriesSlice.js
+++ src/store/countriesSlice.js
@@ -13,38 +13,50 @@ export const getAllCountries = createAsyncThunk(
 export const fetchCountries = createAsyncThunk(
   'countries/fetchCountries',
   async (countries) => {
-    const listOfCountries = await fetch(`https://restcountries.com/v3.1/alpha?codes=${countries}`).then(resp => resp.json());
+    const listOfCountries = await fetch(
+      `https://restcountries.com/v3.1/alpha?codes=${countries}`
+    ).then((resp) => resp.json());
     return listOfCountries;
   }
 );
 
 const initialState = {
-  countriesToRender: ['ger','usa','bra','isl','afg','ala','alb','012'],
+  countriesToRender: ['ger', 'usa', 'bra', 'chi', 'rus', 'par', 'arg', 'esp'],
   fetchedCountries: [],
   allCountries: [],
-  isCountrySelected : false,
+  isCountrySelected: false,
+  countrySelected: '',
 };
 
 const countriesSlice = createSlice({
   name: 'countriesSelection',
   initialState,
-  reducers : {
-    setCountriesToRender : (state, action) => {
+  reducers: {
+    setCountriesToRender: (state, action) => {
       state.countriesToRender = action.payload;
     },
-    setIfCountrySelected : (state) => {
+    setIfCountrySelected: (state) => {
       state.isCountrySelected = !state.isCountrySelected;
-    }
+    },
+    setCountrySelected: (state, action) => {
+      state.countrySelected = action.payload;
+    },
   },
   extraReducers: (builder) => {
     builder.addCase(getAllCountries.fulfilled, (state, action) => {
       state.allCountries.push(action.payload);
     });
     builder.addCase(fetchCountries.fulfilled, (state, action) => {
-      state.fetchedCountries = action.payload;
+      state.fetchedCountries = action.payload.sort((a, b) =>
+        a.name.common.localeCompare(b.name.common)
+      );
     });
   },
 });
 
 export default countriesSlice.reducer;
-export const {setCountriesToRender, setIfCountrySelected} = countriesSlice.actions;
+export const {
+  setCountriesToRender,
+  setIfCountrySelected,
+  setCountrySelected,
+} = countriesSlice.actions;
