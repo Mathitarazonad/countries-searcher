ccfad1b222132e797118fcd76d674f38d0aa4a40
Mathias Tarazona <vladitarazona@gmail.com>
2022-11-04 18:06:00 -0300

    Added to filter countries by the region

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -1,18 +1,25 @@
 import React from 'react';
 import { IoIosClose } from 'react-icons/io';
+import { useDispatch } from 'react-redux';
+import { setRegionSelected } from '../store/countriesSlice';
 
 function Filter() {
+  const dispatch = useDispatch();
+  const handleChange = e => {
+    dispatch(setRegionSelected(e.target.value))
+  };
+
   return (
     <div className='filter-container'>
-      <select name='regions' className='region-select' defaultValue={'all'}>
+      <select name='regions' className='region-select' defaultValue={'all'} onChange={handleChange}>
         <option value='all' disabled hidden>
           Search by Region
         </option>
-        <option value='africa'>Africa</option>
-        <option value='america'>America</option>
-        <option value='asia'>Asia</option>
-        <option value='europe'>Europe</option>
-        <option value='oceania'>Oceania</option>
+        <option value='Africa'>Africa</option>
+        <option value='Americas'>America</option>
+        <option value='Asia'>Asia</option>
+        <option value='Europe'>Europe</option>
+        <option value='Oceania'>Oceania</option>
       </select>
       <IoIosClose id='close-icon'/>
     </div>
