869b419a6c9123e2205785d9110e1a142a5575e2
Mathias Tarazona <vladitarazona@gmail.com>
2022-11-07 14:54:11 -0300

    Added btn click handler to change the theme of the page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
 import {TbMoon} from 'react-icons/tb';
+import { useDispatch } from 'react-redux';
+import changeTheme from '../store/themeSlice';
 
 function Header () {
+  const dispatch = useDispatch();
+
+  const handleClick = () => {
+    dispatch(changeTheme());
+  }
+
   return(
     <header>
       <h2>Where in the world?</h2>
