9f624df8702e0a7115653b02c0a124e2bb1b8d43
Mathias Tarazona <vladitarazona@gmail.com>
2022-11-07 11:27:37 -0300

    Added the handler for the changes of input and update the searched country state with it

--- src/components/Browser.jsx
+++ src/components/Browser.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import { FaSearchLocation } from 'react-icons/fa';
+import { useDispatch } from 'react-redux';
+import { setSearchedCountry } from '../store/countriesSlice';
 
 function Browser() {
+  const dispatch = useDispatch();
+
+  const handleChange = (e) => {
+    dispatch(setSearchedCountry(e.target.value));
+  };
+
   return (
     <div className="browser-container">
       <FaSearchLocation />
-      <input placeholder="Search for a country..."></input>
+      <input placeholder="Search for a country..." onChange={handleChange}></input>
     </div>
   );
 }
