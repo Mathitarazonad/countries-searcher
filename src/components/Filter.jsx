6de020e153087801d20815786d220c3b15ff3182
Mathias Tarazona <vladitarazona@gmail.com>
2022-11-08 16:24:31 -0300

    Refactored the filter, deleted the select tag to make easier the styles, change the handler to a handler of options and display options depending on the state

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -1,38 +1,48 @@
 import React from 'react';
 import { IoIosClose } from 'react-icons/io';
+import { BiChevronDown } from 'react-icons/bi'
 import { useDispatch, useSelector } from 'react-redux';
-import { setRegionSelected } from '../store/countriesSlice';
+import { setRegionSelected, setIfFilterOpen } from '../store/countriesSlice';
 
 function Filter() {
   const dispatch = useDispatch();
   const regionSelected = useSelector((store) => store.countries.regionSelected);
+  const isFilterOpen = useSelector((store) => store.countries.isFilterOpen);
 
-  const handleChange = (e) => {
-    dispatch(setRegionSelected(e.target.value));
+  const handleFilterState = () => {
+    dispatch(setIfFilterOpen());
+  }
+
+  const handleOption = (option) => {
+    dispatch(setRegionSelected(option));
+    dispatch(setIfFilterOpen());
   };
 
   const handleReset = () => {
     dispatch(setRegionSelected('all'));
-  }
+    if(isFilterOpen) {
+      dispatch(setIfFilterOpen());
+    } 
+  };
 
   return (
     <div className="filter-container">
-      <select
-        name="regions"
-        className="region-select"
-        onChange={handleChange}
-        value={regionSelected}
-      >
-        <option value="all" disabled hidden>
-          Search by Region
-        </option>
-        <option value="Africa">Africa</option>
-        <option value="Americas">America</option>
-        <option value="Asia">Asia</option>
-        <option value="Europe">Europe</option>
-        <option value="Oceania">Oceania</option>
-      </select>
-      <IoIosClose id="close-icon" onClick={() => handleReset()} />
+      <div className='filter-bar'>
+        <p onClick={() => handleFilterState()}>{regionSelected === 'all' ? 'Filter by Region' : regionSelected}</p>
+        <div className='filter-icons'>
+          <BiChevronDown id='dropdown-icon' onClick={() => handleFilterState()}/>
+          <IoIosClose id="close-icon" onClick={() => handleReset()} />
+        </div>
+      </div> 
+      {isFilterOpen && <div className='regions-list'>
+        <ul>
+          <li onClick={() => handleOption('Africa')}>Africa</li>
+          <li onClick={() => handleOption('Americas')}>Americas</li>
+          <li onClick={() => handleOption('Asia')}>Asia</li>
+          <li onClick={() => handleOption('Europe')}>Europe</li>
+          <li onClick={() => handleOption('Oceania')}>Oceania</li>
+        </ul>
+      </div>}
     </div>
   );
 }
