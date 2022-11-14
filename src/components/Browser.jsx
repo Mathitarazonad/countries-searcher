9304a475949f0f52a5d5589b0e45fe47c1dd0dca
Mathias Tarazona <vladitarazona@gmail.com>
2022-11-14 16:39:33 -0300

    Reset the className of animations when filter of browser are being used

--- src/components/Browser.jsx
+++ src/components/Browser.jsx
@@ -1,16 +1,15 @@
 import React from 'react';
 import { FaSearchLocation } from 'react-icons/fa';
-import { useDispatch, useSelector } from 'react-redux';
-import { setCurrentPage, setSearchedCountry } from '../store/countriesSlice';
+import { useDispatch } from 'react-redux';
+import { setCurrentPage, setSearchedCountry, setPageAnimation } from '../store/countriesSlice';
 
 function Browser() {
   const dispatch = useDispatch();
-  const currentPage = useSelector(store => store.countries.currentPage);
-  const availablePages = useSelector(store => store.countries.availablePages);
 
   const handleChange = (e) => {
     dispatch(setSearchedCountry(e.target.value));
     dispatch(setCurrentPage(0));
+    dispatch(setPageAnimation(''));
   };
 
   return (
--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -7,6 +7,7 @@ import {
   setIfFilterOpen,
   setAvailablePages,
   setCurrentPage,
+  setPageAnimation,
 } from '../store/countriesSlice';
 
 function Filter() {
@@ -16,6 +17,7 @@ function Filter() {
 
   const handleFilterState = () => {
     dispatch(setIfFilterOpen());
+    dispatch(setPageAnimation(''));
   };
 
   const handleOption = (option) => {
