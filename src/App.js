9aa3476f2475d1af8d84b33b18edfe1c3c556a1d
Mathias Tarazona <vladitarazona@gmail.com>
2022-11-15 09:11:54 -0300

    Edited some things on the footer

--- src/App.js
+++ src/App.js
@@ -50,8 +50,8 @@ function App() {
         </div>
       </main>
       <footer>
-        Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
-        Coded by <a href="#">Mathías Tarazona</a>.
+        Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank" rel='noopener noreferrer' >Frontend Mentor</a>. 
+        Coded by <a href="https://www.frontendmentor.io/profile/Mathitarazonad" target='_blank' rel='noopener noreferrer' >Mathías Tarazona</a>.
       </footer>
     </div>
   );
